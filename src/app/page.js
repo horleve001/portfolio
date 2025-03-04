6082c1baa1b146f9087b53d8511290771c1b33cd
Horvath Levente <horleve001@gmail.com>
2025-03-05 00:11:38 +0100

    Styling home page + adding JSON File for about details

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,18 @@
+import details from "./details";
+
 export default function Home() {
   return (
-    <div className="container">
-      <h1>Üdvözöllek a honlapomon!</h1>
+    <div className=" mt-15 p-5 m-7">
+      <h1 className="text-center">Üdvözöllek a honlapomon!</h1>
       <p>
-        <img className="myself" src="1937.png" alt="Horváth Levente" />
+        <img
+          className="w-4/10 h-4/10 rounded-full inline-block p-3 float-right"
+          src="bonobo.jpg"
+          alt="Horváth Levente"
+        />
       </p>
-      <p>Itt egy bemutatkozás</p>
-      <p>Itt egy bemutatkozás</p>
-      <p>Itt egy bemutatkozás</p>
-      <p>Itt egy bemutatkozás</p>
-      <p>Itt egy bemutatkozás</p>
-      <p>Itt egy bemutatkozás</p>
-      <p>Itt egy bemutatkozás</p>
-      <p>Itt egy bemutatkozás</p>
-      <p>Itt egy bemutatkozás</p>
-      <p>Itt egy bemutatkozás</p>
-      <p>Itt egy bemutatkozás</p>
+      <p className="p-3">{details.about}</p>
+      <p className="p-3">{details.recruiters}</p>
     </div>
   );
 }
