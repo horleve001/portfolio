39a533f2e6fbe14dbf4b8af52dc455d5fa9de529
Horvath Levente <horleve001@gmail.com>
2025-03-07 00:37:06 +0100

    Styling

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -7,11 +7,12 @@ export default function Contact() {
       <p>Telefon: +36 30 4057845</p>
       <p>Email:horleve001@gmail.com</p>
       <p>LinkedIn</p>
-      <div className="bg-amber-300 hover:bg-blue-800 display: inline-block p-3 text-white">
-        <Link href="https://www.linkedin.com/in/levente-horvath-horleve001/">
-          <p>Ugrás a LinkedIn profilomra</p>
-        </Link>
-      </div>
+      <Link
+        href="https://www.linkedin.com/in/levente-horvath-horleve001/"
+        className="bg-amber-300 hover:bg-blue-800 display: inline-block p-3 text-white"
+      >
+        <p>Ugrás a LinkedIn profilomra</p>
+      </Link>
       <p>Github</p>
       <Link
         href="https://github.com/horleve001"
